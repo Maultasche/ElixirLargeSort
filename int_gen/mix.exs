744ca39311f083189e260787d1808822f9b51873
Maultasche <kevin.peter@gmail.com>
2019-07-12 13:08:48 -0700

    Set IntGen version number to 1.0.0

--- int_gen/mix.exs
+++ int_gen/mix.exs
@@ -4,7 +4,7 @@ defmodule IntGen.MixProject do
   def project do
     [
       app: :int_gen,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
