d44331f82edb6d7eb749d89124ec61336db99b05
Maultasche <kevin.peter@gmail.com>
2019-07-17 14:38:02 -0700

    Implemented gen_file_name to generate names for gen files

--- int_sort/lib/int_sort.ex
+++ int_sort/lib/int_sort.ex
@@ -1,18 +1,49 @@
 defmodule IntSort do
   @moduledoc """
-  Documentation for IntSort.
+  Contains functionality for sorting and chunking integers as well as merging the
+  chunk files
   """
+
+  @integer_file Application.get_env(:int_sort, :integer_file)
+
+  # @doc """
+  # Chunks an integer file and writes the sorted chunks to chunk files
+
+  # ## Parameters
+
+  # - input_file: the path to the file to be read
+  # - output_dir: the path to the directory where the output files are to written
+  # - chunk_size: the size of the chunks to be created
+  # - gen: the generation number to be used for the chunk files
+  # ## Returns
+
+  # :ok
+  # """
+  # def create_chunk_files(input_file, output_dir, chunk_size, gen) do
+  #   #Create a stream pipeline that reads in integers from the input stream,
+  #   #chunks them, sorts them, and then writes the chunks to files
+  #   @integer_file.create_integer_file_stream(input_file)
+  #     |> @integer_file.read_stream()
+  #     |> Chunk.create_chunks(chunk_size)
+  #     |> Chunk.sort_chunks()
+  #     |> Chunk.write_chunks_to_separate_streams(gen, )
+
+  # end
 
   @doc """
-  Hello world.
+  Creates an intermediate file name based on a generation and chunk number
 
-  ## Examples
+  ## Parameters
 
-      iex> IntSort.hello()
-      :world
+  - gen: the generation the file is associated with
+  - num: the chunk number assocatied with the file
 
+  ## Returns
+
+  A file name containing the gen and chunk number
   """
-  def hello do
-    :world
+  @spec gen_file_name(non_neg_integer(), non_neg_integer()) :: String.t()
+  def gen_file_name(gen, num) do
+    "gen#{gen}-#{num}.txt"
   end
 end
--- int_sort/test/int_sort_test.exs
+++ int_sort/test/int_sort_test.exs
@@ -2,7 +2,10 @@ defmodule IntSortTest do
   use ExUnit.Case
   doctest IntSort
 
-  # test "greets the world" do
-  #   assert IntSort.hello() == :world
-  # end
+  describe "gen_file_name -" do
+    test "Create a gen file name" do
+      file_name = IntSort.gen_file_name(3, 7)
+
+      assert file_name == "gen3-7.txt"
+    end
 end
